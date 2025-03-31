dbc4c3f909a1f05522a250ce161edfc21a72a0f2
Subhadip Das <subhadipdas911@gmail.com>
2025-03-31 13:01:50 +0530

    How to perform Basic Authorization using User Name and Password

--- src/test/java/Authorization/BasicAuth/BasicAuthorization.java
+++ src/test/java/Authorization/BasicAuth/BasicAuthorization.java
@@ -14,6 +14,7 @@ public class BasicAuthorization {
 		
 		RestAssured.baseURI = "https://postman-echo.com";
 		
+//		Non Preemptive :- 
 		Response responsePay = given()
 			.auth()
 			.basic("postman", "password")
@@ -24,6 +25,18 @@ public class BasicAuthorization {
 		
 		System.out.println("Status Code : "+responsePay.statusCode());
 		responsePay.getBody().prettyPrint();
-
+		
+//		Preemptive :-
+		
+		Response responsePay1 = given()
+				.auth()
+				.preemptive()
+				.basic("postman", "password")
+			.when()
+				.get("/basic-auth")
+			.then()
+				.extract().response();
+		System.out.println("Status Code 1 : "+responsePay1.statusCode());
+		responsePay1.getBody().prettyPrint();
 	}
 }
