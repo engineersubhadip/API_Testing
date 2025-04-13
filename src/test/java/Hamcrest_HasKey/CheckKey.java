0abb4b09a1e45064205fd9605d03403904aeedeb
Subhadip Das <subhadipdas911@gmail.com>
2025-04-13 11:27:52 +0530

    If a particular key is present or not

--- src/test/java/Hamcrest_HasKey/CheckKey.java
+++ src/test/java/Hamcrest_HasKey/CheckKey.java
@@ -12,12 +12,12 @@ import io.restassured.path.json.JsonPath;
 
 public class CheckKey {
 	
-	@Test
-	public void test_CheckForKey() {
+	@Test(priority=2)
+	public void test_CheckForKey1() {
 		RestAssured.baseURI = "https://restful-booker.herokuapp.com/booking/1";
 		
 		String response =  given().when().get().then()
-		.body("bookingdates", Matchers.hasKey("checkout"))
+		.body("bookingdates", Matchers.hasKey("checkin"))
 		.extract().response().asString();
 		
 		JsonPath jp = new JsonPath(response);
@@ -25,6 +25,14 @@ public class CheckKey {
 		Map<String, String> bookingDates = jp.getMap("bookingdates");
 		
 		System.out.println("Size of bookingDates : "+bookingDates.keySet().size());
+	}
+	
+	@Test
+	public void test_CheckForKey() {
+		RestAssured.baseURI = "https://restful-booker.herokuapp.com/booking/1";
+		
+		given().when().get().then()
+		.body("$", Matchers.hasKey("firstname"));
 	}
 
 }
